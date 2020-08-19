5ef0bd477ac4dc72450480bf48b44c64981e5f2c
Anna-Pinewood <65508302+Anna-Pinewood@users.noreply.github.com>
2020-08-19 12:38:55 +0300

    Add files via upload
    something's wrong with the extension of data drom file.

--- main.c
+++ main.c
@@ -1,20 +1,62 @@
+
+
+#include <windows.h>
 #include <stdio.h>
-#include <stdlib.h>
-//#include <string.h>
-int main()
+#include <string.h>
+#include <wingdi.h>
+/*typedef struct tagBITMAPINFOHEADER
 {
-    FILE *MESSAGE;
-    MESSAGE = fopen("message.txt","r");
-    char *buf;
-    fseek(MESSAGE, 0, SEEK_END);
-    long size = ftell(MESSAGE);
-    buf = malloc(size*sizeof(char));
-    if (buf == NULL) {
-        printf(stderr, "Memory mistake");
-        return 1;
+    DWORD  biSize;
+    LONG   biWidth;
+    LONG   biHeight;
+    WORD   biPlanes;
+    WORD   biBitCount;
+    DWORD  biCompression;
+    DWORD  biSizeImage;
+    LONG   biXPelsPerMeter;
+    LONG   biYPelsPerMeter;
+    DWORD  biClrUsed;
+    DWORD  biClrImportant;
+} BITMAPINFOHEADER, *PBITMAPINFOHEADER;
+
+   typedef struct tagRGBTRIPLE {
+    BYTE rgbtBlue;
+    BYTE rgbtGreen;
+    BYTE rgbtRed;
+  } RGBTRIPLE;*/
+int main(){
+    int i, j;
+    FILE * IMG = fopen("img.BMP", "rb");
+    BITMAPFILEHEADER  header1;
+    PBITMAPFILEHEADER * ptr_header1;
+    BITMAPINFOHEADER  header2;
+    PBITMAPINFOHEADER * ptr_header2;
+    RGBTRIPLE rgb;
+    fseek(IMG, 392, SEEK_SET);
+    fread(header2.biWidth, sizeof(int), 1, IMG);
+    fread(header2.biHeight, sizeof(int), 1, IMG);
+    int buf_size = header2.biWidth*header2.biHeight;
+    RGBTRIPLE buf[buf_size]; // ???
+    fseek(IMG, 10, SEEK_SET);
+    fread(header1.bfOffBits, sizeof(DWORD), 1, IMG);
+    fseek(IMG, 10, SEEK_SET);
+    for (i=0; i < buf_size; i++) {
+        for (j=0; j<3; j++) {
+            if (j==0)  fread(rgb.rgbtRed, sizeof(BYTE), 1, IMG);
+            else if (j==1) fread(rgb.rgbtGreen, sizeof(BYTE), 1, IMG);
+            else fread(rgb.rgbtBlue, sizeof(BYTE), 1, IMG);
+            RGBTRIPLE temp[1] = {rgb};
+        }
+        memcpy(buf, &rgb, sizeof(RGBTRIPLE));
     }
-    int rc;
-    for (int i = 0; (rc=getc(MESSAGE)) != EOF && i < size; buf[i++] = rc);
-for (int i = 0; i < size; i++) printf("buf[%d] = %d \n", i ,buf[i]);
-return 0;
+  /* for (i=0; i<buf_size; i++){
+        for (j=0; j<3; j++) {
+            if (j==0)  printf("%d", rgb.rgbtRed);
+            else if (j==1) printf("%d", rgb.rgbtGreen);
+            else printf("%d", rgb.rgbtBlue);
+        }
+    }*/
+    fclose(IMG);
+
+    return 0;
 }
